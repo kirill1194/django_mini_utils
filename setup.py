eb3b076fb97c1b5ec266d15d458406bc72a2a05c
Кирилл <kirill.altuhov@gmail.com>
2018-01-17 17:56:10 +0500

    rename

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from os.path import join, dirname
 
 setup(
-    name='django-mini-utils',
+    name='mini_utils',
     version='0.1',
     packages=find_packages(),
     long_description=open(join(dirname(__file__), 'README.rst')).read(),
